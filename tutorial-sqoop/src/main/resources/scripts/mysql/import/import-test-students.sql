8ff11bd47d191f5376c691798ce4fce5b75c47bc
Tedmond Dong <tedmonddong@tedmonds-mbp.t-mobile.com>
2016-10-04 09:51:23 -0700

    remove from git as it added incorrectly

--- tutorial-sqoop/src/main/resources/scripts/mysql/import/import-test-students.sql
+++
@@ -1,2 +0,0 @@
-truncate table splice.students;
-call syscs_util.import_data('SPLICE','students',null,'<TUTORIAL-DIR>/data/test/students/',null,null,null,null,null,-1,'<TUTORIAL-DIR>/status/test/students/',null,null);
